390b96ec36ee15b4bb8bbb362f66e3c95e3a5d93
Frapassa <40418742+Frapassa@users.noreply.github.com>
2019-05-22 10:18:47 +0200

    Add files via upload

---
+++ Altri.cs
@@ -0,0 +1,32 @@
+﻿using Shell32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PileSwLib.UtilTest
+{
+    public class Altro:UtilTestLib
+    {
+        public static int GetCestino()
+        {
+
+            int itemsCount=0;
+            Shell shell = new Shell();
+            Folder recycleBin = shell.NameSpace(10);
+            try
+            {
+
+                itemsCount = recycleBin.Items().Count;
+                // int hresult = SHQueryRecycleBin(String.Empty, ref sqrbi);
+            }
+            catch (Exception ex)
+            {
+                log.Error(ex);
+            }
+
+            return itemsCount;
+          
+        }
+    }
+}
